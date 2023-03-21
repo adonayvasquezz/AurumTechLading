2465675f30bea7638bb30e5516bcd91111c9dabd
Jesus Vasquez <adonay16.vz@gmail.com>
2023-03-21 01:44:04 -0600

    feat: presentation message timeout

--- src/components/home/Presentation/Presentation.tsx
+++ src/components/home/Presentation/Presentation.tsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import css from "./Presentation.module.css";
+import { PresentationInfo } from "../../../utils/PresentationData";
 
 const Presentation = () => {
-  return <div className={css.container}>Presentation</div>;
+  const [timeoutState, setTimeoutState] = useState<NodeJS.Timer>();
+  const [messageCounter, setMessageCounter] = useState(0);
+  const [PresentationData] = useState(PresentationInfo);
+  useEffect(() => {
+    setTimeoutState(
+      setTimeout(() => {
+        setMessageCounter((counter) => counter + 1);
+        if (messageCounter >= PresentationData.length - 1) {
+          setMessageCounter(0);
+        }
+      }, 3000)
+    );
+
+    return () => clearTimeout(timeoutState);
+  }, [messageCounter]);
+
+  return (
+    <>
+      <div className={css.container}>
+        <h1>
+          Development of
+          <span className={css.inputLabel}>
+            {" "}
+            {PresentationData[messageCounter].input}{" "}
+          </span>
+          software that
+          <span className={css.outputLabel}>
+            {" "}
+            {PresentationData[messageCounter].output}{" "}
+          </span>
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default Presentation;
---
+++ src/utils/PresentationData.ts
@@ -0,0 +1,10 @@
+export interface presentationMessages {
+  input: string;
+  output: string;
+}
+
+export const PresentationInfo: presentationMessages[] = [
+  { input: "fully custom", output: "astonish your audience" },
+  { input: "high quality", output: "meets your needs" },
+  { input: "high scalable", output: "streamlines your business" },
+];
