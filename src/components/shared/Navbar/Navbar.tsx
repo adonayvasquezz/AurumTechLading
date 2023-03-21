f5db3919723fa05e3a4769b565492c9a5b88382d
Jesus Vasquez <adonay16.vz@gmail.com>
2023-03-20 23:46:48 -0600

    feat: navbar layout

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import css from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <div className={css.container}>
+        <div>ICON</div>
+        <div className={css.navContainer}>
+          <span>About</span>
+          <span>Services</span>
+          <span>Contact us</span>
+        </div>
+      </div>
     </>
   );
 };
